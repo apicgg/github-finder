02c2e78c3dfc0c75dd2ec0a96e7e4001fcfc646d
Anurag Pramanik <p.anurag@outlook.com>
2022-04-17 00:05:11 +0530

    final commit

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -3,9 +3,6 @@ import githubReducer from './GithubReducer'
 
 const GithubContext = createContext()
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
@@ -15,83 +12,6 @@ export const GithubProvider = ({ children }) => {
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
-
-  // Search users with github api
-  const searchUsers = async (text) => {
-    setLoading()
-
-    const params = new URLSearchParams({
-      q: text,
-    })
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    })
-
-    const { items } = await response.json()
-
-    dispatch({
-      type: 'GET_USERS',
-      payload: items,
-    })
-  }
-
-  // Get single user with github api
-  const getUser = async (login) => {
-    setLoading()
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    })
-
-    if (response.status === 404) {
-      window.location = '/notfound'
-    } else {
-      const data = await response.json()
-
-      dispatch({
-        type: 'GET_USER',
-        payload: data,
-      })
-    }
-  }
-
-  // Get user repos
-  const getUserRepos = async (login) => {
-    setLoading()
-
-    const params = new URLSearchParams({
-      sort: 'created',
-      per_page: 10,
-    })
-
-    const response = await fetch(
-      `${GITHUB_URL}/users/${login}/repos?${params}`,
-      {
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-        },
-      }
-    )
-
-    const data = await response.json()
-
-    dispatch({
-      type: 'GET_REPOS',
-      payload: data,
-    })
-  }
-
-  // Clear search results
-  const clearUsers = () => {
-    dispatch({
-      type: 'CLEAR_USERS',
-    })
-  }
 
   // Get user from api (for testing only)
   // Add useEffect in UserResults.jsx t o display it
@@ -112,22 +32,11 @@ export const GithubProvider = ({ children }) => {
   //   })
   // }
 
-  // Set loading
-  const setLoading = () => {
-    dispatch({ type: 'SET_LOADING' })
-  }
-
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        user: state.user,
-        repos: state.repos,
-        searchUsers,
-        clearUsers,
-        getUser,
-        getUserRepos,
+        ...state,
+        dispatch,
       }}
     >
       {children}
